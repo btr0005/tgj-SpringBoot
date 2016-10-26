e34f2598d5e8b5e59d05ffa50a1552e5ababc98a
Brenden <brenden.romanowski@sentar.com>
2016-10-26 13:19:10 -0500

    Added join component with a simple form

---
+++ Angular2App/app/join.component.ts
@@ -0,0 +1,59 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'join-component',
+  template: `
+			
+				<h3>Become a Member</h3>
+				
+				<div class="panel panel-default">
+				  <div class="panel-body" >
+					<h4 [style.color]="member.color">
+						<span class="glyphicon glyphicon-user"></span>
+						{{member.name}}
+					</h4>
+				  </div>
+				</div>				
+				
+				<form>
+					<div class="form-group">
+						<label for="name">Name</label>
+						<input type="text" class="form-control" id="name" 
+							required
+							[(ngModel)]="member.name" name="name"
+							#name="ngModel">
+					</div>
+					<div class="form-group">
+						<label for="color">Favorite Color</label>
+						<input type="color" class="form-control" id="color" 
+							required
+							[(ngModel)]="member.color" name="color"
+							#color="ngModel">
+					</div>
+				 
+					<div align="right">
+						<button type="submit" class="btn btn-primary"
+								(click)="newMember()">Join</button>
+					</div>
+				</form>
+			`,
+})
+
+export class JoinComponent { 
+	submitted = false;
+	
+	member = new Member("New User","#000000");
+	
+	onSubmit() { this.submitted = true;  }
+	
+	newMember() {
+		console.log("New Member: " + this.member.name);
+	}
+}
+
+export class Member {
+	constructor(
+		public name: string,
+		public color: string
+	){}
+}
