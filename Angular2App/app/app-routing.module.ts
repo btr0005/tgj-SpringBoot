8e220036e130adb63a81c54fba25deb2773d5436
Brenden <brenden.romanowski@sentar.com>
2016-11-01 11:59:21 -0500

    Moved routing to separate module

---
+++ Angular2App/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule }     from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { JoinComponent } from './join.component';
+import { MemberListComponent } from './memberlist.component';
+
+@NgModule({
+	imports: [
+		RouterModule.forRoot([
+			{
+				path: 'join',
+				component: JoinComponent
+			},
+			{
+				path: 'members',
+				component: MemberListComponent
+			}
+		])
+	],
+	exports: [
+		RouterModule
+	]
+})
+export class AppRoutingModule {}
--- Angular2App/app/app.module.ts
+++ Angular2App/app/app.module.ts
@@ -1,40 +1,23 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { JoinComponent } from './join.component';
 import { MemberListComponent } from './memberlist.component';
 import { MembersService } from './members.service';
+
 import { StoryComponent } from './story.component';
 
 @NgModule({
   imports:  [ 
 		BrowserModule,
 		FormsModule,
-		HttpModule,
-		Ng2BootstrapModule,
-		RouterModule.forRoot([
-			{
-				path: '',
-				component: MemberListComponent
-			},
-			{
-				path: 'join',
-				component: JoinComponent
-			},
-			{
-				path: 'members',
-				component: MemberListComponent
-			},
-			{
-				path: 'story',
-				component: StoryComponent
-			}
-		])
+		//StoryModule,
+		AppRoutingModule,
 	],
   providers: [
 		MembersService
