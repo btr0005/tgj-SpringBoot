032a582992cf6c82153d2227f66ad9f6553f27a6
Brenden <brenden.romanowski@sentar.com>
2016-11-01 12:59:15 -0500

    Removing .js and .js.map files as they are not source files but compiled files

--- Angular2App/app/members/mock-members.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.MEMBERS = [
-    { name: 'Anthony Fantano', color: '#008833' },
-    { name: 'Paul Right There', color: '#FF33FF' },
-    { name: 'Chart Studios 2', color: '#FF7700' }
-];
-//# sourceMappingURL=mock-members.js.map
