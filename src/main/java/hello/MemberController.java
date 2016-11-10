7b445927faf7d864bb5ff82acfd98dbdb771122c
Brenden <brenden.romanowski@sentar.com>
2016-11-10 13:51:47 -0600

    Removed the DB Facade class which was unnecessary

--- src/main/java/hello/MemberController.java
+++ src/main/java/hello/MemberController.java
@@ -1,41 +1,31 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
 @RestController
 @RequestMapping("/members")
 public class MemberController {
-
-    private final AtomicLong counter = new AtomicLong();
 	
-	private DatabaseFacade dbFacade = new DatabaseFacade(new MembersServiceFake());
+	private MembersService memberService = new MembersServiceFake();
 
 	@CrossOrigin()
     @RequestMapping(method=RequestMethod.POST)
     public String join(@RequestBody Member member) {
-		String response;
-	
-		if (this.dbFacade.addMember(member))
-			response = "New member added to db!";
+		if (this.memberService.addMember(member))
+			return "New member added to db!";
 		else
-			response = "Failed to add member to db!";
-		
-		return response;
+			return "Failed to add member to db!";
     }
 	
 	@CrossOrigin()
 	@RequestMapping(method=RequestMethod.GET)
 	public Collection<Member> members() {
-		return this.dbFacade.getMembers();
+		return this.memberService.getMembers();
     }
 
 }
--- src/main/java/hello/StoryController.java
+++ src/main/java/hello/StoryController.java
@@ -1,13 +1,10 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
@@ -15,31 +12,27 @@ import java.util.Collection;
 @RequestMapping("/stories")
 public class StoryController {
 
-	private DatabaseFacade dbFacade = new DatabaseFacade(new StoriesServiceFake());
+	private StoriesService storiesService = new StoriesServiceFake();
 
 	@CrossOrigin()
     @RequestMapping(method=RequestMethod.POST)
     public String create(@RequestBody Story story) {
-		String response;
-	
-		if (this.dbFacade.addStory(story))
-			response = "New story added to db!";
+		if (this.storiesService.addStory(story))
+			return "New story added to db!";
 		else
-			response = "Failed to add story to db!";
-		
-		return response;
+			return "Failed to add story to db!";
     }
 	
 	@CrossOrigin()
 	@RequestMapping(method=RequestMethod.GET)
 	public Collection<Story> getStories() {
-		return this.dbFacade.getStories();
+		return this.storiesService.getStories();
     }
 	
 	@CrossOrigin()
 	@RequestMapping(method=RequestMethod.GET, value = "/{storyId}")
 	public Story getStory(@PathVariable int storyId) {
-		return this.dbFacade.getStory(storyId);
+		return this.storiesService.getStory(storyId);
     }
 
 }
